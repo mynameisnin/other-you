316dc7946e5ff1fc763af086c1e6e2863073c2de
kaypopClon <147307700+kaypop123@users.noreply.github.com>
2025-03-28 23:23:31 +0900

    아담 버프스킬 구현
    아담 버프스킬 구현

---
+++ Assets/AdamAttackSpeedBuff.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using UnityEngine;
+
+public class AdamAttackSpeedBuff : MonoBehaviour
+{
+    [Header("Buff Settings")]
+    public float attackSpeedMultiplier = 1.5f;
+    public float buffDuration = 5f;
+    public int manaCost = 20;
+
+    [Header("Effect")]
+    public GameObject buffEffect;
+
+    private bool isBuffActive = false;
+    private Coroutine buffCoroutine;
+
+    private AdamMovement adamMovement;
+    private Animator adamAnimator;
+
+    void Start()
+    {
+        adamMovement = GetComponent<AdamMovement>();
+        adamAnimator = GetComponent<Animator>();
+
+        // ? Animator 파라미터 초기화 (중요!)
+        if (adamAnimator != null)
+        {
+            adamAnimator.SetFloat("attackSpeedMultiplier", 1f);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TryActivateBuff();
+        }
+    }
+
+    public void TryActivateBuff()
+    {
+        if (isBuffActive)
+        {
+            Debug.Log("버프 이미 활성화 중");
+            return;
+        }
+
+        if (!PlayerStats.Instance.HasEnoughMana(manaCost))
+        {
+            Debug.Log("마나 부족");
+            ManaBarUI.Instance?.FlashBorder();
+            return;
+        }
+
+        PlayerStats.Instance.ReduceMana(manaCost);
+
+        if (buffCoroutine != null)
+            StopCoroutine(buffCoroutine);
+
+        buffCoroutine = StartCoroutine(ApplyBuff());
+    }
+
+    private IEnumerator ApplyBuff()
+    {
+        isBuffActive = true;
+
+        // 기존 값 저장
+        float originalAttackDelay = adamMovement.attackInputCooldown;
+
+        // 적용
+        adamMovement.attackInputCooldown *= 1f / attackSpeedMultiplier;
+        adamAnimator.SetFloat("attackSpeedMultiplier", attackSpeedMultiplier); // ? 애니메이터 파라미터로 조절
+
+        if (buffEffect != null)
+            buffEffect.SetActive(true);
+
+        Debug.Log("?? 공격 속도 + 애니메이션 속도 버프 적용!");
+
+        yield return new WaitForSeconds(buffDuration);
+
+        // 복원
+        adamMovement.attackInputCooldown = originalAttackDelay;
+        adamAnimator.SetFloat("attackSpeedMultiplier", 1f);
+
+        if (buffEffect != null)
+            buffEffect.SetActive(false);
+
+        isBuffActive = false;
+        Debug.Log("? 공격 속도 버프 종료");
+    }
+}
