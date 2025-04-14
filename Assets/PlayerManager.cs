29f4307487113bac0fc39ac6bce8428174de3436
chess351 <park030501@gmail.com>
2025-04-14 19:15:17 +0900

    Revert "Merge branch 'main' into DungeonTilemap"
    This reverts commit 1d31557d9f6478acb079a378d9bfbe8352dbff2e, reversing
    changes made to e2345a13e0384473ee7c5b8d7edcaeedda5e0dc2.

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
@@ -16,16 +16,6 @@ public class PlayerManager : MonoBehaviour
         else
         {
             Destroy(gameObject); //  중복 생성 방지
-        }
-    }
-    void Start()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
-        
-        // 특정 씬 이름에 들어가면 자기 자신 삭제
-        if (currentScene.name == "startscens") 
-        {
-            Destroy(gameObject);
         }
     }
 }
