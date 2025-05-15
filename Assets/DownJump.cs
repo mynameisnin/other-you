944b2bb0b3f50db6c719031a17fb319a81ec0ef0
chess351 <park030501@gmail.com>
2025-05-15 23:15:25 +0900

    하향점프 수정

--- Assets/DownJump.cs
+++ Assets/DownJump.cs
@@ -4,31 +4,26 @@ using UnityEngine;
 
 public class DownJump : MonoBehaviour
 {
-    private Collider2D platformCollider;
-    private AdamMovement player; // 플레이어 스크립트 참조
+    private BoxCollider2D platformCollider;
 
     void Start()
     {
-        platformCollider = GetComponent<Collider2D>();
-        player = FindObjectOfType<AdamMovement>(); // 플레이어 찾기
+        platformCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        if (IsPlayerOnPlatform() && Input.GetKey(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(DisableCollision());
-        }
+ 
+
     }
 
-    bool IsPlayerOnPlatform()
+    public void TriggerDownJump()
     {
-        return player != null && player.isGround; // 메서드 호출 X, 변수이므로 괄호 제거
+        StartCoroutine(DisableCollision());
     }
 
     System.Collections.IEnumerator DisableCollision()
     {
-        player.isGround = false; // 바닥 체크 해제 (중요!)
         platformCollider.enabled = false; // 충돌 해제 (바닥 통과)
 
         yield return new WaitForSeconds(0.5f); // 0.3초 후 다시 충돌 활성화
