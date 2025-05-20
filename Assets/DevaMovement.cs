26f7aceed24c69c0f33dd02871aefaeb3a73c771
HAN DONG YEON <147307700+kaypop123@users.noreply.github.com>
2025-05-20 16:42:44 +0900

    아담 데바 버그 수정 및 다이어로그 수정
    아담 데바 버그 수정 및 다이어로그 수정

--- Assets/DevaMovement.cs
+++ Assets/DevaMovement.cs
@@ -81,6 +81,9 @@ public class DebaraMovement : MonoBehaviour
         {
             CastLaserSkill();
         }
+        DebaraAnime.SetBool("isGrounded", isGround);
+
+        
     }
 
     float currentSpeed = 0f;
