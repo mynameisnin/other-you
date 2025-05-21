df53c1f47d6b6f5364cc7665816b56e34661351b
The chickencode <dsa03215@naver.com>
2025-05-22 00:11:58 +0900

    맵빌리지 타임라인 사운드 시작지점 수정
    맵빌리지 타임라인 사운드 시작지점 수정

--- Assets/TimelineEventReceiver.cs
+++ Assets/TimelineEventReceiver.cs
@@ -8,6 +8,7 @@ public class TimelineEventReceiver : MonoBehaviour
     {
         if (earthquakeSfx != null && !earthquakeSfx.isPlaying)
         {
+            earthquakeSfx.time = 28f;
             earthquakeSfx.Play();
             Debug.Log("타임라인 Signal에서 지진 효과음 재생!");
         }
